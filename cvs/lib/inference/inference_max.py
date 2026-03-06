4adfdabe6a5337508a361d21cf5e77eead5c73b4
Rajesh Thummala <rthummal_amdeng>
2026-03-06 16:01:21 -0600

    Chnages related to inference max test

--- cvs/lib/inference/inference_max.py
+++ cvs/lib/inference/inference_max.py
@@ -21,7 +21,7 @@ class InferenceMaxJob(InferenceBaseJob):
 
     def get_server_script_directory(self):
         """InferenceMAX scripts are in the cloned repo."""
-        return '/app/InferenceX/benchmarks/single_node'
+        return '/app/InferenceX'
 
     def get_result_filename(self):
         """InferenceMAX result filename."""
