81af470dc156a334bb348280aab052d131c795e6
Rajesh Thummala <rthummal_amdeng>
2026-04-03 12:42:42 -0500

    Chnages related ro Rocm module load change

--- cvs/tests/inference/pytorch_xdit/pytorch_xdit_flux1_dev_single.py
+++ cvs/tests/inference/pytorch_xdit/pytorch_xdit_flux1_dev_single.py
@@ -274,6 +274,7 @@ def hf_token(inference_dict):
 def s_phdl(cluster_dict):
     """Create and return a command execution handle for all cluster nodes."""
     node_list = list(cluster_dict['node_dict'].keys())
+    env_vars = cluster_dict.get("env_vars")
 
     # Single-node mode: execute locally ONLY when the target actually refers to this machine.
     #
@@ -292,6 +293,7 @@ def s_phdl(cluster_dict):
             user=cluster_dict.get("username"),
             password=cluster_dict.get("password"),
             pkey=cluster_dict.get("priv_key_file"),
+            env_vars=env_vars,
         )
 
     log.info(f"Using parallel-ssh execution mode for {len(node_list)} node(s)")
@@ -301,6 +303,7 @@ def s_phdl(cluster_dict):
         user=cluster_dict.get('username'),
         password=cluster_dict.get('password'),
         pkey=cluster_dict.get('priv_key_file'),
+        env_vars=env_vars,
     )
 
 
