c2df174b18431dc0581a1c7c2641b399cfa46632
amd-droy <droy@amd.com>
2026-06-02 15:47:57 -0400

    updating benchmark tests-7
    Signed-off-by: amd-droy <droy@amd.com>

--- cvs/lib/sglang_disagg_lib.py
+++ cvs/lib/sglang_disagg_lib.py
@@ -776,7 +776,6 @@ class SglangDisaggPD:
         # ------------------------------------------------------------------
         cmd = f'''docker exec {self.container_name} /bin/bash -c  "
                       mkdir -p {self.log_dir}/benchmark_node; \
-                      pip install --upgrade --no-deps 'sglang[all]' --quiet && \
                       cd /sgl-workspace/sglang/benchmark/gsm8k && \
                       source /tmp/benchmark_env_script.sh && \
                       nohup python3 ./bench_sglang.py \
@@ -894,6 +893,7 @@ class SglangDisaggPD:
                       mkdir -p {self.log_dir}/benchmark_node; \
                       source /tmp/benchmark_env_script.sh && \
                       pip install --upgrade --no-deps sglang --quiet &&  \
+                      pip list | grep -E "torch|torchvision|sglang" && \
                       python3 -m sglang.bench_serving --backend {i_dict['backend']} \
                       --dataset-name random \
                       --num-prompts {i_dict['num_prompts']} \
